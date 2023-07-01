c582cce2d670dbb1aace02de07fe10aba5ec5c06
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-01 13:49:32 +0530

    Print Message in different colors

--- 02.print.sh
+++ 02.print.sh
@@ -9,9 +9,9 @@ echo hello world
 #COL -stands for color code
 #possible colors are RED 31, Green 32, yellow 33, blue 34,magenta 35,cyan 36
 
-echo -e "\[31m Hello world in Red color\e[0m"
-echo -e "\[32m Hello world in Green color\e[0m"
-echo -e "\[33m Hello world in Yellow color\e[0m"
-echo -e "\[34m Hello world in Blue color\e[0m"
-echo -e "\[35m Hello world in Magenta color\e[0m"
-echo -e "\[36m Hello world in Cyan color\e[0m"
+echo -e "\[31m Hello world in Red color \ e[0m"
+echo -e "\[32m Hello world in Green color \e[0m"
+echo -e "\[33m Hello world in Yellow color \e[0m"
+echo -e "\[34m Hello world in Blue color \e[0m"
+echo -e "\[35m Hello world in Magenta color \e[0m"
+echo -e "\[36m Hello world in Cyan color \e[0m"
