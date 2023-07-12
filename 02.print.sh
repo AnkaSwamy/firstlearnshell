89b2c9e28d0e50636de2fef2d3307d3f8aafb528
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-12 16:24:30 +0530

    using variables...

--- 02.print.sh
+++ 02.print.sh
@@ -15,3 +15,11 @@ echo -e "\e[33m Hello world in Yellow color \e[0m"
 echo -e "\e[34m Hello world in Blue color \e[0m"
 echo -e "\e[35m Hello world in Magenta color \e[0m"
 echo -e "\e[36m Hello world in Cyan color \e[0m"
+
+sample() {
+  echo hello
+  return 1
+  echo bye
+}
+sample
+echo fun exit status $?
