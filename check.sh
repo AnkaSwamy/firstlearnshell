dc0822e38927cdf8297fd7525e0fbb8f2734d5a0
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-07-12 23:11:00 +0530

    topics

---
+++ check.sh
@@ -0,0 +1,7 @@
+echo hello_world
+echo -e "\e[31m Hello world in Red color \e[0m"
+echo -e "\e[32m Hello world in Green color \e[0m"
+echo -e "\e[33m Hello world in Yellow color \e[0m"
+echo -e "\e[34m Hello world in Blue color \e[0m"
+echo -e "\e[35m Hello world in Magenta color \e[0m"
+echo -e "\e[36m Hello world in Cyan color \e[0m"
